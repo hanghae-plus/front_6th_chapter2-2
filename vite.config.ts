6bcd0652964409d274072d48ca6eaab6106751fe
YeongseoYoon <l112xpla@gmail.com>
2025-08-03 04:07:23 +0900

    chore: svgr 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 export default mergeConfig(
   defineConfig({
-    plugins: [react()],
+    plugins: [
+      react(),
+      svgr({
+        svgrOptions: {
+          exportType: "default",
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
   })
-)
+);
