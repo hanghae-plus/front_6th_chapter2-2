c47a5871aca16dc7560635c41fe95b91216c27d6
Angie Lee <angielxx94@gmail.com>
2025-08-04 12:43:33 +0900

    chore: prettier 설정 및 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    resolve: {
+      alias: {
+        "@": "/src",
+        "@components": "/src/components",
+        "@hooks": "/src/hooks",
+        "@utils": "/src/utils",
+        "@types": "/src/types",
+        "@constants": "/src/constants",
+        "@models": "/src/models",
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
   })
-)
+);
