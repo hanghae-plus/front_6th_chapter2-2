f8a5da5532167e18e949ae04fbd9e065a839a0fa
tksemf7410 <tksemf7410@gmail.com>
2025-08-04 19:34:08 +0900

    refactor: ProductListSummary 컴포넌트 분리

--- src/basic/components/CartPage.tsx
+++ src/basic/components/CartPage.tsx
@@ -3,6 +3,7 @@ import { ProductWithUI } from "../App";
 import { CartItem, Coupon } from "../../types";
 import { formatPrice } from "../utils/formatters";
 import { getRemainingStock } from "../models/cart";
+import { ProductListSummary } from "./ui/ProductListSummary";
 
 // TODO: 장바구니 페이지 컴포넌트
 // 힌트:
@@ -226,12 +227,7 @@ export function CartPage({
       <div className="lg:col-span-3">
         {/* 상품 목록 */}
         <section>
-          <div className="mb-6 flex justify-between items-center">
-            <h2 className="text-2xl font-semibold text-gray-800">전체 상품</h2>
-            <div className="text-sm text-gray-600">
-              총 {products.length}개 상품
-            </div>
-          </div>
+          <ProductListSummary products={products} />
           {filteredProducts.length === 0 ? (
             <div className="text-center py-12">
               <p className="text-gray-500">
