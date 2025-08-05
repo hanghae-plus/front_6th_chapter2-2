5ab17334a9fe59b81e601b7b1b80f3ffdbf0db6a
Hwirin-Kim <kimhuirin@gmail.com>
2025-08-05 20:33:48 +0900

    fix : formatPrice 기능 분리

--- src/refactoring(hint)/utils/hooks/useDebounce.ts
+++ src/refactoring(hint)/utils/hooks/useDebounce.ts
@@ -6,6 +6,23 @@
 //
 // 사용 예시: 검색어 입력 디바운싱
 
+import { useEffect, useState } from "react";
+
+// export function useDebounce<T>(value: T, delay: number): T {
+//   // TODO: 구현
+
+// }
 export function useDebounce<T>(value: T, delay: number): T {
-  // TODO: 구현
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
 }
