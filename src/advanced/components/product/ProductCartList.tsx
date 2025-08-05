944d6ead4ce94272905e6264c2c5692e4076e222
JHeeJinDev <zqewe12@gmail.com>
2025-08-05 18:05:50 +0900

    refactor: ProductCartList Jotai 사용으로 인한 리팩토링

--- src/advanced/components/product/ProductCartList.tsx
+++ src/advanced/components/product/ProductCartList.tsx
@@ -1,32 +1,22 @@
-import { ProductWithUI } from "../../types";
+import { useAtom } from "jotai";
+import { filteredProductsAtom, getRemainingStockAtom } from "../../store";
 import { ProductCartItem } from "./ProductCartItem";
 
-interface ProductCartListProps {
-  filteredProducts: ProductWithUI[];
-  getRemainingStock: (product: ProductWithUI) => number;
-  handleAddToCart: (product: ProductWithUI) => void;
-  isAdmin: boolean;
-}
+export const ProductCartList = () => {
+  const [filteredProducts] = useAtom(filteredProductsAtom);
+  const [getRemainingStock] = useAtom(getRemainingStockAtom);
 
-export const ProductCartList = ({
-  filteredProducts,
-  getRemainingStock,
-  handleAddToCart,
-  isAdmin,
-}: ProductCartListProps) => {
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {filteredProducts.map((product) => {
-          const remainingStock = getRemainingStock(product);
+          const remainingStock = getRemainingStock(product.id);
 
           return (
             <ProductCartItem
               key={product.id}
               product={product}
               remainingStock={remainingStock}
-              onAddToCart={handleAddToCart}
-              isAdmin={isAdmin}
             />
           );
         })}
