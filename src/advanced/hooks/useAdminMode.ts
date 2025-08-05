a0eef716bf4cd9781b6598629b832885338956b2
JHeeJinDev <zqewe12@gmail.com>
2025-08-05 17:59:13 +0900

    refactor: hook 폴더 삭제

--- src/advanced/hooks/useAdminMode.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export function useAdminMode() {
-  const [isAdmin, setIsAdmin] = useState(false);
-  const handleToggleAdmin = () => {
-    setIsAdmin((prev) => !prev);
-  };
-  return { isAdmin, handleToggleAdmin };
-}
