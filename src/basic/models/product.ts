13d4aaa65645ce8877ff7273e857a13ab311532a
annkimm <zizicoo208@naver.com>
2025-08-06 19:24:44 +0900

    refactor: remove dont' need function model product

--- src/basic/models/product.ts
+++ src/basic/models/product.ts
@@ -1,35 +1,5 @@
 import { ProductWithUI } from '../types'
 
-export const isRecommendedProduct = (product: ProductWithUI) => {
-  return product.isRecommended
-}
-
-export const isStock = (product: ProductWithUI) => {
-  return product.stock > 0
-}
-
-export const isDiscounts = (product: ProductWithUI) => {
-  return product.discounts.length > 0
-}
-
-export const getMaxDiscountRate = (product: ProductWithUI) => {
-  if (product.discounts.length === 0) return 0
-  const rates = product.discounts.map((item) => item.rate)
-  return Math.max(...rates)
-}
-
-export const isRemainingRange = (
-  min: number,
-  max: number,
-  remainingStock: number,
-) => {
-  return remainingStock <= max && remainingStock > min
-}
-
-export const isOutOfStock = (remainingStock: number) => {
-  return remainingStock <= 0
-}
-
 export const getFilteredProducts = (
   searchTerm: string,
   products: ProductWithUI[],
