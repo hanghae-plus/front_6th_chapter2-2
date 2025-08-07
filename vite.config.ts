df5c34fd57dfb271e6b733a485a3dfaa5fc3d083
Jaehwan Choi <lieblichoi@gmail.com>
2025-08-08 01:14:41 +0900

    Deploy: 배포

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react-swc';
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: '/front_6th_chapter2-2/',
+    build: {
+      rollupOptions: {
+        input: {
+          index: 'index.basic.html',
+        },
+      },
+      outDir: 'dist',
+    },
   }),
   defineTestConfig({
     test: {
