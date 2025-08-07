798cff6af94b5f499a32808c1f2ab6a530d7fd00
Amelia-Shin <gmldnjs0402@naver.com>
2025-08-07 19:37:57 +0900

    refactor: Admin page에 있는 상품|쿠폰 Tab 컴포넌트로 분리

--- src/basic/App.tsx
+++ src/basic/App.tsx
@@ -18,6 +18,7 @@ import {
   ShoppingBagIcon,
   LargeShoppingBagIcon,
 } from './components/icons';
+import { TabLayout } from './components/ui/layout/TabLayout';
 
 const App = () => {
   const { notifications, addNotification, setNotifications } = useNotification();
@@ -149,6 +150,10 @@ const App = () => {
     setShowProductForm(true);
   };
 
+  const handleActiveTab = (value: 'products' | 'coupons') => {
+    setActiveTab(value);
+  };
+
   const totals = calculateCartTotal();
 
   return (
@@ -170,28 +175,7 @@ const App = () => {
               <p className='text-gray-600 mt-1'>상품과 쿠폰을 관리할 수 있습니다</p>
             </div>
             <div className='border-b border-gray-200 mb-6'>
-              <nav className='-mb-px flex space-x-8'>
-                <button
-                  onClick={() => setActiveTab('products')}
-                  className={`py-2 px-1 border-b-2 font-medium text-sm transition-colors ${
-                    activeTab === 'products'
-                      ? 'border-gray-900 text-gray-900'
-                      : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
-                  }`}
-                >
-                  상품 관리
-                </button>
-                <button
-                  onClick={() => setActiveTab('coupons')}
-                  className={`py-2 px-1 border-b-2 font-medium text-sm transition-colors ${
-                    activeTab === 'coupons'
-                      ? 'border-gray-900 text-gray-900'
-                      : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
-                  }`}
-                >
-                  쿠폰 관리
-                </button>
-              </nav>
+              <TabLayout activeTab={activeTab} handleActiveTab={handleActiveTab} />
             </div>
 
             {activeTab === 'products' ? (
