0aeb5118b031bf1125a65c5aab18ffd1a5e530c0
Dan <gunw.dan@gmail.com>
2025-08-09 00:53:52 +0900

    feat: add deploy settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
+const base: string = process.env.NODE_ENV === 'production' ? '/front_6th_chapter2-1/' : '';
+
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
+      },
+    },
+    base,
+    build: {
+      rollupOptions: {
+        input: 'index.advanced.html',
       },
     },
   }),
